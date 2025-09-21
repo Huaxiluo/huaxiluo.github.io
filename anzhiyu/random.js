209b925fbd168dc06fa72df101bb0ce271bcea5e
Huaxiluo <huaxiluolove@qq.com>
2025-09-21 18:55:57 +0800

    Site updated: 2025-09-21 18:55:56

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/09/16/hello-world/","2025/09/17/test/"];function toRandomPost(){
+var posts=["2025/09/17/test/","2025/09/16/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
